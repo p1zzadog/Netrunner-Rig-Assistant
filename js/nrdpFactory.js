6f5636cd44b2d85528e322e7105a2c477fe58fcb
Student <student@RU-103.local>
2015-09-28 17:54:59 -0600

    extracted static cardlist and setlists from nrdb, linked to angular factory

--- js/nrdpFactory.js
+++ js/nrdpFactory.js
@@ -1,7 +1,53 @@
-angular.module('nrdpApp').factory('nrdpFactory', [function(nrdpFactory) {
+angular.module('nrdpApp').factory('nrdpFactory', ['$http', function($http) {
 
+	// console.log('allCards', allCards)
+	// console.log('allSets', allSets)
+	// console.log('hofScrape', hofScrape)
+	// console.log('corelist', corelist)
 
-	return {
-		null:null
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// NRDB API CALLS - TO BE IMPLEMENTED IN THE FUTURE
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+	// $http({
+	// 	method: 'GET',
+	// 	url: 'http://netrunnerdb.com/api/cards/',
+	// 	cache: true,
+	// }).then(function(response) {
+	// 	allCards = response.data;
+	// 	console.log(allCards)
+	// });
+
+	// $http({
+	// 	method: 'GET',
+	// 	url: 'http://netrunnerdb.com/api/sets/',
+	// 	cache: true,
+	// }).then(function(response) {
+	// 	allSets = response.data;
+	// 	console.log(allSets)
+	// });
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// SORT CARDS INTO SETS
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	for (var i=0; i<allSets.length; i++) {
+		allSets[i].cards = [];
+		for (var j=0; j<allCards.length; j++) {
+			if (allCards[j].set_code === allSets[i].code) { 
+				allSets[i].cards.push(allCards[j].code);
+			}
+		}
 	}
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// EXTRACT DECKLIST IDs FROM HALL OF FAME SCRAPE
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	
+	return {
+		allSets : allSets,
+		allCards : allCards,
+		hofScrape : hofScrape,
+		corelist : corelist
+	}
+
 }]);
--- js/nrdpTroller.js
+++ js/nrdpTroller.js
@@ -1,4 +1,9 @@
 angular.module('nrdpApp', ['ui.bootstrap']);
 angular.module('nrdpApp').controller('nrdpTroller', ['$scope', 'nrdpFactory', function($scope, nrdpFactory) {
 
+	$scope.allSets = nrdpFactory.allSets;
+
+	console.log($scope.allSets)
+
+
 }]);
