cd0f599c7a75c41ef72f5580b5965ef02cac4262
Student <student@RU-103.local>
2015-10-01 15:18:36 -0600

    cleaned up controller code

--- js/nrdpTroller.js
+++ js/nrdpTroller.js
@@ -1,36 +1,26 @@
 angular.module('nrdpApp', ['ui.bootstrap', 'ngAnimate']);
 angular.module('nrdpApp').controller('nrdpTroller', ['$scope', '$modal', 'nrdpFactory', function($scope, $modal, nrdpFactory) {
 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Initialize Data Arrays
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 	$scope.allSets = nrdpFactory.allSets;
-	$scope.allDecklists = nrdpFactory.allDecklists;
+	allDecklists = nrdpFactory.allDecklists;
 	var userCardList = [];
-	// console.log('decklists', $scope.allDecklists);
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// Accordion View
+// Accordion View Initialize
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	
 	$scope.chooseSetsOpen = true;
 	$scope.suggestionsOpen = false;
 
-	// $scope.openSets = function() {
-	// 	$scope.chooseSetsOpen = true;
-	// 	$scope.suggestionsOpen = false;
-	// }
-
-	// $scope.openSuggestions = function() {
-	// 	$scope.chooseSetsOpen = false;
-	// 	$scope.suggestionsOpen = true;
-	// }
-
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// Functions creating an array of all cards a user owns based on which data packs they specify
+// Applies styles to number of core buttons, automaticaly selects core set
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-	
-
 	
 	$scope.setCores = function(n){
-
-		console.log('setCores' + n)
-		$scope.numberOfCores = n;
 
 		switch(n) {
 			case 1:
@@ -60,9 +50,12 @@ angular.module('nrdpApp').controller('nrdpTroller', ['$scope', '$modal', 'nrdpFa
 		$scope.allSets[2].userSelect = true;
 	};
 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Data pack select toggle and select button
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-	
+
 	$scope.toggleSet = function(index) {
 		$scope.allSets[index].userSelect = !$scope.allSets[index].userSelect
-
 	};
 
 	$scope.selectAll = function() {
@@ -77,30 +70,34 @@ angular.module('nrdpApp').controller('nrdpTroller', ['$scope', '$modal', 'nrdpFa
 		});
 	};
 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Function creates an array of all cards a user owns based on which data packs they specify
+// Function then calls makeSuggestion(); to populate an array of decklists the user has cards for
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-	
+
 	$scope.selectSets = function() {
-		var cards = []
+		userCardList = []
 		if (allSets[2].userSelect !== true) {
-
 			$scope.selectACore=true;
-
 		}
 
 		else {
-
+			$scope.selectACore=false;
 			for (var i=0; i<allSets.length; i++) {
-				if (allSets[i].userSelect === true) {
-					allSets[i].cards.forEach(function(element){
-						cards.push(element);
+
+				if (allSets[i].userSelect === true) {					
+					
+					allSets[i].cards.forEach(function(card){
+						userCardList.push(card);
 					});
 				};		
 			};
 
-			userCardList = cards;
-			// $scope.chooseSetsOpen = !$scope.chooseSetsOpen;
-			$scope.makeSuggestion();
+			makeSuggestion();
 			$scope.suggestionsOpen = true;
 		};		
 	};
+
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // Utility function to return max value of an array
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -111,27 +108,27 @@ angular.module('nrdpApp').controller('nrdpTroller', ['$scope', '$modal', 'nrdpFa
 
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// Suggestion making algorithm, leaves out quantity of cards per pack
+// Suggestion making algorithm, leaves out quantity of cards per pack and number of cores
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-	$scope.makeSuggestion = function() {
+	var makeSuggestion = function() {
 
 		// filters allDecklists array into a deckSuggestions array, a decklist object is passed if all cards appear in the user's cardlist
-		$scope.deckSuggestions = $scope.allDecklists.filter(function(decklist) {
+		$scope.deckSuggestions = allDecklists.filter(function(decklist) {
 			var keysArray = Object.keys(decklist.cards);
 
 			for (var j=0; j<keysArray.length; j++) {
 				if (userCardList.indexOf(keysArray[j]) === -1) {
 					return false;
-				}
-			}
+				};
+			};
 			return true;
 		});
 
 
 		// creates an array of card IDs in order to attach card names to numbers
 		$scope.deckSuggestions.forEach(function(decklist, index){
-			$scope.deckSuggestions[index].cardIdList = Object.keys(decklist.cards)
+			$scope.deckSuggestions[index].cardIdList = Object.keys(decklist.cards);
 		});
 
 		// creates an array of card names with same index as card IDs array
@@ -162,21 +159,21 @@ angular.module('nrdpApp').controller('nrdpTroller', ['$scope', '$modal', 'nrdpFa
 	$scope.openModal = function(deckIndex, cardIndex) {
 		var allCardsIndex = _.findIndex(nrdpFactory.allCards, {"code" : cardIndex});
 		$scope.imagePlaceholder = nrdpFactory.allCards[allCardsIndex].imagesrc;
-		$scope.cardInfo = nrdpFactory.allCards[allCardsIndex]
-		console.log($scope.cardInfo)
+		$scope.cardInfo = nrdpFactory.allCards[allCardsIndex];
 
 		var modalInstance = $modal.open({
 			animation: $scope.animationsEnabled,
 			templateUrl: 'cardInfoModal.html',
 			controller: 'ModalInstanceCtrl',
 			scope: $scope
-		})
-	}
+		});
+	};
 
 
 
 }]);
 
+// needed to make a separate controller for modal instances in order to close modal and load images correctly since modal is outside normal scope
 angular.module('nrdpApp').controller('ModalInstanceCtrl', function ($scope, $modalInstance) {
 
 	$scope.closeModal = function () {
